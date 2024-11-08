66988b9bcfb3f252c160ebd723d23db0f7ceae8c
guitiorr <officialglude12345@gmail.com>
2024-11-08 21:45:30 +0700

    install bootstrap + add navbar component

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\MainPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/main', function (){
+//     return view('main-page');
+// });
+
+Route::get('/main', [MainPageController::class, 'index']);
