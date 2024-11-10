6a44852ba3d4036ce642201c2e7ca6aaebea46aa
guitiorr <officialglude12345@gmail.com>
2024-11-10 16:46:32 +0700

    UPDATE PostFactory : add body faker and add category_id factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -19,7 +20,9 @@ class PostFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'body' => fake()->sentence(),
             'author_id' => User::factory(),
+            'category_id' => Category::factory(),
         ];
     }
 }
