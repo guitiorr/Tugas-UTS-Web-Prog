8a694fb9ae8d3de40352e3bfa0228c8a35994f27
guitiorr <officialglude12345@gmail.com>
2024-11-10 12:37:46 +0700

    UPDATE Category Model: add post belongsTo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
@@ -13,4 +14,8 @@ class Category extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function post(): BelongsTo{
+        return $this->belongsTo(Post::class, 'category_id');
+    }
 }
