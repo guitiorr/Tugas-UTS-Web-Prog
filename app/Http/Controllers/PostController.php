c88917a84dfb88f7286586a76c1680c6de642091
guitiorr <officialglude12345@gmail.com>
2024-11-10 18:37:23 +0700

    add view post detail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,7 +32,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('post', ['post' => $post]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [PostController::class, 'index']);
+
+Route::get('/post/{post:id}', [PostController::class, 'show']);
