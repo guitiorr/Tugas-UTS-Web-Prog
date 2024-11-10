919b3981ad375015694cf15dd74ef49633cf3700
guitiorr <officialglude12345@gmail.com>
2024-11-10 16:32:02 +0700

    UPDATE Category Seeder = add contents for category table

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'name' => 'Web Dev'
+        ]
+        );
+
+        Category::create([
+            'name' => 'AI'
+        ]
+        );
+
+        Category::create([
+            'name' => 'Blockchain'
+        ]
+        );
     }
 }
